f7d9a2f21d85e4d3d4fdfe2010ff73e4f52b250d
Brenno <151473774+brenno1206@users.noreply.github.com>
2025-08-11 10:40:38 -0300

    Update Contato.cs

--- Exercicios01/Contato.cs
+++ Exercicios01/Contato.cs
@@ -1,7 +1,15 @@
 namespace PrimeiroProjeto
 {
-    public class Contato(string numero)
+    public class Contato(string nome, string numero, string apelido, int quantoMeDeve)
     {
-        public string Numero = numero;
+        public string Nome { get; set; } = nome;
+
+        public string Numero { get; set; } = numero;
+
+        public string Apelido { get; set; } = apelido;
+
+        public int QuantoMeDeve { get; set; } = quantoMeDeve;
+
     }
+    
 }
